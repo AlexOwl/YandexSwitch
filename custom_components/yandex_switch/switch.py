5473c30bc2de1a18f1e637184458932d72db905e
AlexOwl <AlexOwl@protonmail.com>
2020-02-23 22:29:06 +0300

    Change schema names and implement switch properties

--- custom_components/yandex_switch/switch.py
+++ custom_components/yandex_switch/switch.py
@@ -3,7 +3,7 @@ import voluptuous as vol
 from datetime import timedelta
 import asyncio
 
-from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
+from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA, ATTR_TODAY_ENERGY_KWH, ATTR_CURRENT_POWER_W, DEVICE_CLASS_OUTLET
 from homeassistant.const import CONF_NAME, CONF_ICON, CONF_HOST, CONF_DEVICE_ID, CONF_SCAN_INTERVAL
 from homeassistant.helpers.event import track_time_interval
 import homeassistant.helpers.config_validation as cv
@@ -18,7 +18,7 @@ CONF_SWITCH_ID = 'switch_id'
 
 # REAL SCHEMA: [{"mode":"rw","code":"switch_1","name":"开关1","property":{"type":"bool"},"iconname":"icon-dp_power2","id":1,"type":"obj","desc":""},{"mode":"rw","code":"countdown_1","name":"开关1倒计时","property":{"unit":"s","min":0,"max":86400,"scale":0,"step":1,"type":"value"},"iconname":"icon-dp_time2","id":9,"type":"obj","desc":""},{"mode":"rw","code":"add_ele","name":"增加电量","property":{"unit":"","min":0,"max":50000,"scale":3,"step":100,"type":"value"},"iconname":"icon-battery","id":17,"type":"obj","desc":""},{"mode":"ro","code":"cur_current","name":"当前电流","property":{"unit":"mA","min":0,"max":30000,"scale":0,"step":1,"type":"value"},"iconname":"icon-Ele","id":18,"type":"obj","desc":""},{"mode":"ro","code":"cur_power","name":"当前功率","property":{"unit":"W","min":0,"max":50000,"scale":1,"step":1,"type":"value"},"iconname":"icon-dp_tool","id":19,"type":"obj","desc":""},{"mode":"ro","code":"cur_voltage","name":"当前电压","property":{"unit":"V","min":0,"max":5000,"scale":1,"step":1,"type":"value"},"iconname":"icon-a_function_turbo","id":20,"type":"obj","desc":""},{"mode":"ro","code":"test_bit","name":"产测结果位","property":{"unit":"","min":0,"max":5,"scale":0,"step":1,"type":"value"},"iconname":"icon-dp_direction","id":21,"type":"obj","desc":""},{"mode":"ro","code":"voltage_coe","name":"电压校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":22,"type":"obj","desc":""},{"mode":"ro","code":"electric_coe","name":"电流校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":23,"type":"obj","desc":""},{"mode":"ro","code":"power_coe","name":"功率校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":24,"type":"obj","desc":""},{"mode":"ro","code":"electricity_coe","name":"电量校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":25,"type":"obj","desc":""},{"mode":"ro","code":"fault","scope":"fault","name":"故障告警","property":{"label":["ov_cr"],"type":"bitmap","maxlen":1},"id":26,"type":"obj","desc":"ov_cr：过流保护"}]
 
-SCHEMA_DEFAULT = [{"mode":"rw","code":"switch","name":"开关1","property":{"type":"bool"},"iconname":"icon-dp_power2","id":1,"type":"obj","desc":""},{"mode":"rw","code":"countdown","name":"开关1倒计时","property":{"unit":"s","min":0,"max":86400,"scale":0,"step":1,"type":"value"},"iconname":"icon-dp_time2","id":9,"type":"obj","desc":""},{"mode":"rw","code":"add_ele","name":"增加电量","property":{"unit":"","min":0,"max":50000,"scale":3,"step":100,"type":"value"},"iconname":"icon-battery","id":17,"type":"obj","desc":""},{"mode":"ro","code":"cur_current","name":"当前电流","property":{"unit":"mA","min":0,"max":30000,"scale":0,"step":1,"type":"value"},"iconname":"icon-Ele","id":18,"type":"obj","desc":""},{"mode":"ro","code":"cur_power","name":"当前功率","property":{"unit":"W","min":0,"max":50000,"scale":1,"step":1,"type":"value"},"iconname":"icon-dp_tool","id":19,"type":"obj","desc":""},{"mode":"ro","code":"cur_voltage","name":"当前电压","property":{"unit":"V","min":0,"max":5000,"scale":1,"step":1,"type":"value"},"iconname":"icon-a_function_turbo","id":20,"type":"obj","desc":""},{"mode":"ro","code":"test_bit","name":"产测结果位","property":{"unit":"","min":0,"max":5,"scale":0,"step":1,"type":"value"},"iconname":"icon-dp_direction","id":21,"type":"obj","desc":""},{"mode":"ro","code":"voltage_coe","name":"电压校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":22,"type":"obj","desc":""},{"mode":"ro","code":"electric_coe","name":"电流校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":23,"type":"obj","desc":""},{"mode":"ro","code":"power_coe","name":"功率校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":24,"type":"obj","desc":""},{"mode":"ro","code":"electricity_coe","name":"电量校准系数","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"iconname":"icon-gaodiyin","id":25,"type":"obj","desc":""},{"mode":"ro","code":"fault","scope":"fault","name":"故障告警","property":{"label":["ov_cr"],"type":"bitmap","maxlen":1},"id":26,"type":"obj","desc":"ov_cr：过流保护"}]
+SCHEMA_DEFAULT = [{"mode":"rw","code":"switch","property":{"type":"bool"},"id":1},{"mode":"rw","code":"countdown","property":{"unit":"s","min":0,"max":86400,"scale":0,"step":1,"type":"value"},"id":9},{"mode":"rw","code":"statistics_function","property":{"unit":"","min":0,"max":50000,"scale":3,"step":100,"type":"value"},"id":17},{"mode":"ro","code":"current_current_mA","property":{"unit":"mA","min":0,"max":30000,"scale":0,"step":1,"type":"value"},"id":18},{"mode":"ro","code":ATTR_CURRENT_POWER_W,"property":{"unit":"W","min":0,"max":50000,"scale":1,"step":1,"type":"value"},"id":19},{"mode":"ro","code":"current_voltage_v","property":{"unit":"V","min":0,"max":5000,"scale":1,"step":1,"type":"value"},"id":20},{"mode":"ro","code":"test_bit","property":{"unit":"","min":0,"max":5,"scale":0,"step":1,"type":"value"},"id":21},{"mode":"ro","code":"voltage_coefficient","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"id":22},{"mode":"ro","code":"electric_coefficient","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"id":23},{"mode":"ro","code":"power_coefficient","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"id":24},{"mode":"ro","code":"electricity_coefficient","property":{"unit":"","min":0,"max":1000000,"scale":0,"step":1,"type":"value"},"id":25},{"mode":"ro","code":"overcurrent_protection","scope":"fault","property":{"label":["ov_cr"],"type":"bitmap","maxlen":1},"id":26}]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME): cv.string,
@@ -85,11 +85,12 @@ class YandexSwitchDevice(SwitchDevice):
 
     @property
     def is_on(self):
-        return self._status.get(self._tuya_client.switch_id, False)
+        return self._status.get(self._tuya_client.switch_id) or False
 
     @property
     def device_state_attributes(self):
-        return helper.dps_to_value(self._schema, self._status)
+        if self._status.get(self._tuya_client.switch_id) is not None:
+            return helper.dps_to_value(self._schema, self._status)
 
     @property
     def icon(self):
@@ -103,3 +104,16 @@ class YandexSwitchDevice(SwitchDevice):
 
     async def async_update(self):
         await self._tuya_client.send_control_request()
+
+    @property
+    def current_power_w(self):
+        return self.device_state_attributes.get(ATTR_CURRENT_POWER_W)
+
+    @property
+    def today_energy_kwh(self):
+        # TODO
+        return None
+    
+    @property
+    def device_class(self):
+        return DEVICE_CLASS_OUTLET
